e7331f748688d34bf2a56af3088d8b7452fc6626
Matthew Brett <matthew.brett@gmail.com>
2016-03-17 17:29:34 -0700

    DOC: add first pass at docs, with test

--- openfmri/__init__.py
+++ openfmri/__init__.py
@@ -1,4 +1,7 @@
-# Init for consden module
+""" openfmri package
+
+Provides Python interface to OpenFMRI data model
+"""
 
 from ._version import get_versions
 __version__ = get_versions()['version']
